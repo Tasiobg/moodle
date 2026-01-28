f73215d804cc7256512f18c38b4c1b024cb38bc1
Tasio <tasio.bertomeu@moodle.com>
2026-01-28 14:59:51 +0100

    MDL-87769 core: Add optional shortname URL parameter to the web manifest

--- public/admin/tool/mobile/mobile.webmanifest.php
+++ public/admin/tool/mobile/mobile.webmanifest.php
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+declare(strict_types=1);
+
 /**
  * Web manifest for including a native app banner.
  *
@@ -32,8 +34,8 @@ define('NO_MOODLE_COOKIES', true);
 
 require_once(__DIR__ . '/../../../config.php');
 
-header('Content-Type: application/json; charset: utf-8');
-header('Cache-Control: public, max-age=' .  HOURSECS . ', no-transform');
+header('Content-Type: application/manifest+json; charset=utf-8');
+header('Cache-Control: public, max-age=' . HOURSECS . ', no-transform');
 header('Expires: ' . gmdate('D, d M Y H:i:s', time() + HOURSECS) . ' GMT');
 
 $mobilesettings = get_config('tool_mobile');
@@ -41,23 +43,22 @@ $mobilesettings = get_config('tool_mobile');
 if (!empty($CFG->enablemobilewebservice) && !empty($mobilesettings->enablesmartappbanners) &&
         !empty($mobilesettings->androidappid)) {
 
+    $requestedshortname = optional_param('shortname', '', PARAM_TEXT);
     $manifest = new StdClass;
-    $manifest->short_name = format_string($SITE->shortname, true, [
-        'context' => \core\context\system::instance(),
-    ]);
+    $manifest->short_name = $requestedshortname !== '' ? $requestedshortname : $SITE->shortname;
     $manifest->prefer_related_applications = true;
     $manifest->icons = [(object)
         [
             'sizes' => '144x144',
             'type' => 'image/png',
-            'src' => "$CFG->wwwroot/$CFG->admin/tool/mobile/pix/icon_144.png"
-        ]
+            'src' => "$CFG->wwwroot/$CFG->admin/tool/mobile/pix/icon_144.png",
+        ],
     ];
     $manifest->related_applications = [(object)
         [
             'platform' => 'play',
             'id' => $mobilesettings->androidappid,
-        ]
+        ],
     ];
     echo json_encode($manifest);
 }
